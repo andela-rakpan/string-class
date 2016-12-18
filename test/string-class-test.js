bda012152fc734408aa1b36c0b1c3bac0291683a
Raphael Akpan <raphael.akpan@andela.com>
2016-12-18 19:02:59 +0100

    Add toUpper and toLower test suites

--- test/string-class-test.js
+++ test/string-class-test.js
@@ -7,21 +7,48 @@ const assert = chai.assert;
 // Require String Class to Test
 require('../src/string-class.js');
 
-// Test suites for method hasVowels
+// Test suite for method hasVowels
 describe('hasVowels', ()=>{
-  it('should return true if the string contains vowels', ()=>{
+  it('should return true if the string contains vowels', ()=> {
     const errorMessage = 'String contains vowels, should return true';
     assert.equal('Raphael'.hasVowels(), true);
   });
 
-  it('should return false if the string does not contains vowels', ()=>{
+  it('should return false if the string does not contains vowels', ()=> {
     const errorMessage = 'String does not contain vowel, should return false';
     assert.equal('Try'.hasVowels(), false);
   });
 
-  it('should return boolean result', ()=>{
+  it('should return a boolean result', ()=>{
     const errorMessage = 'String does not contain vowel, should return false';
     assert.typeOf('Andela'.hasVowels(), 'boolean');
   });
 });
+
+// Test suite for method toUpper
+describe('toUpper', ()=>{
+  it('should return uppercase characters for a given string', ()=> {
+    const errorMessage = 'String contains vowels, should return true';
+    assert.equal('Raphael'.toUpper(), 'RAPHAEL');
+  });
+
+  it('should return a string result', ()=>{
+    const errorMessage = 'String does not contain vowel, should return false';
+    assert.typeOf('Andela'.toUpper(), 'string');
+  });
+});
+
+// Test suite for method toLower
+describe('toLower', ()=>{
+  it('should return lowercase characters for a given string', ()=> {
+    const errorMessage = 'String contains vowels, should return true';
+    assert.equal('RapHAEl'.toLower(), 'raphael');
+  });
+
+  it('should return a string result', ()=>{
+    const errorMessage = 'String does not contain vowel, should return false';
+    assert.typeOf('Andela'.toLower(), 'string');
+  });
+});
+
 
