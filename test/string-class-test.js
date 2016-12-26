1a58fac9ac17d40089c412472aee1be0107b58a2
Raphael Akpan <raphael.akpan@andela.com>
2016-12-26 04:09:04 +0100

    Add ucFirst test case

--- test/string-class-test.js
+++ test/string-class-test.js
@@ -8,64 +8,77 @@ const assert = chai.assert;
 require('../src/string-class.js');
 
 // Test suite for method hasVowels
-describe('hasVowels', ()=>{
+describe('hasVowels', ()=> {
   it('should return true if the string contains vowels', ()=> {
-    const errorMessage = '\'Raphael\' contains vowels, should return true';
-    assert.equal('Raphael'.hasVowels(), true, errorMessage);
+    const word = 'Raphael';
+    const errorMessage = `'Raphael' contains vowels, should return true`;
+    assert.equal(word.hasVowels(), true, errorMessage);
   });
 
   it('should return false if the string does not contains vowels', ()=> {
-    const errorMessage = '\'Try\' does not contain vowel, should return false';
-    assert.equal('Try'.hasVowels(), false, errorMessage);
+    const word = 'Try';
+    const errorMessage = `'Try' does not contain vowel, should return false`;
+    assert.equal(word.hasVowels(), false, errorMessage);
   });
 
-  it('should return a boolean result', ()=>{
-    const errorMessage = 'Result should be a string value of true or false';
-    assert.typeOf('Andela'.hasVowels(), 'boolean', errorMessage);
+  it('should return a boolean result', ()=> {
+    const word = 'Andela';
+    const errorMessage = 'Result should be a boolean value';
+    assert.typeOf(word.hasVowels(), 'boolean', errorMessage);
   });
 });
 
 // Test suite for method toUpper
-describe('toUpper', ()=>{
+describe('toUpper', ()=> {
   it('should return all characters as uppercase characters', ()=> {
-    const errorMessage = '\'Raphael\' should return \'RAPHAEL\'';
-    assert.equal('Raphael'.toUpper(), 'RAPHAEL', errorMessage);
+    const word = 'Raphael';
+    const result = 'RAPHAEL';
+    const errorMessage = `'Raphael' should return 'RAPHAEL'`;
+    assert.equal(word.toUpper(), result, errorMessage);
   });
 
-  it('should return a string result', ()=>{
+  it('should return a string result', ()=> {
+    const word = 'Andela';
     const errorMessage = 'Result should be a string value';
-    assert.typeOf('Andela'.toUpper(), 'string', errorMessage);
+    assert.typeOf(word.toUpper(), 'string', errorMessage);
   });
 });
 
 // Test suite for method toLower
-describe('toLower', ()=>{
+describe('toLower', ()=> {
   it('should return all characters as lowercase characters', ()=> {
-    const errorMessage = '\'RapHAEl\' should return \'raphael\'';
-    assert.equal('RapHAEl'.toLower(), 'raphael', errorMessage);
+    const word = 'RapHAEl';
+    const result = 'raphael';
+    const errorMessage = `'RapHAEl' should return 'raphael'`;
+    assert.equal(word.toLower(), result, errorMessage);
   });
 
-  it('should return a string result', ()=>{
+  it('should return a string result', ()=> {
+    const word = 'Andela';
     const errorMessage = 'Result should be a string value';
-    assert.typeOf('Andela'.toLower(), 'string', errorMessage);
+    assert.typeOf(word.toLower(), 'string', errorMessage);
   });
 });
 
 // Test suite for method ucFirst
-describe('ucFirst', ()=>{
+describe('ucFirst', ()=> {
   it('should return the first character as uppercase', ()=> {
-    const errorMessage = '\'raphael\' should return \'R\'';
-    assert.equal('raphael'.ucFirst()[0], 'R', errorMessage);
+    const word = 'raphael';
+    const errorMessage = `'raphael' should return 'R' as first character`;
+    assert.equal(word.ucFirst()[0], 'R', errorMessage);
   });
 
-  it('should return other characters after first as lowercase', ()=> {
-    const errorMessage = '\'raPHaEl\' should return \'Raphael\'';
-    assert.equal('raPHaEl'.ucFirst(), 'Raphael', errorMessage);
+  it('should not alter other characters after first character', ()=> {
+    const word = 'raPHaEl';
+    const result = 'RaPHaEl';
+    const errorMessage = `'raPHaEl' should return 'RaPHaEl'`;
+    assert.equal(word.ucFirst(), result, errorMessage);
   });
 
-  it('should return a string result', ()=>{
+  it('should return a string result', ()=> {
+    const word = 'Andela';
     const errorMessage = 'Result should be a string value';
-    assert.typeOf('Andela'.toLower(), 'string', errorMessage);
+    assert.typeOf(word.ucFirst(), 'string', errorMessage);
   });
 });
 
